e4a36e9cf76431588c28cfd432691dab24544eee
Benny Tordrup <bstordrup@gmail.com>
2022-04-20 09:07:45 +0200

    Sample solution and project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            System.ServiceModel.Channels.IChannelListener channelListener = null;
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
